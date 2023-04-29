17a90c5714347003ed56968b69cb84ebc9093b60
DanielUFPE <daniel.sampaio@ufpe.br>
2023-04-29 02:32:18 -0300

    Update

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,53 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
 #include <syslog.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <libgen.h>
 
-int main(int argc, char **argv)
-{
-	int status = -1;
-	FILE *fp;
-	char *writefile, *writestr, *dirc;
+int main(int argc, char *argv[]) {
 
-	openlog(NULL, 0, LOG_USER);
+// create variable argument assignments
+    char *writefile = argv[1];
+    char *writestr  = argv[2];
 
-	if (argc != 3)
-	{
-		syslog(LOG_ERR, "Invalid arguments");	
-		exit(1);
-	}
+// if statement to check number of arguments (arg[0] -> file call, arg[1] -> file name to write, arg[2] -> file content to write)
+// log error, print message, and exit
+    if (argc != 3) {
+        syslog(LOG_ERR, "ERROR : %s requires 3 arguments to function", writefile);
+        exit(1);
+    }
 
-	writefile = argv[1];
-	writestr = argv[2];
+// open file for writing to
+    FILE *f = fopen(writefile, "w");
 
-    if ((writefile[0] == '\0') || (writestr[0] == '\0'))
-		exit(1);
+// safety for general case use where file would not exist -- logs error, prints message, and exit
+    if (!f) {
+	syslog(LOG_ERR, "ERROR : %s file does not exist", writefile);
+	exit(1);
+    }
 
-	dirc = strdup(writefile);
+// write content to file & close file
+    fprintf(f, "%s", writestr);
+    fclose(f);
 
-	status = mkdir(dirname(dirc), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+// show that the content string has been written to the specified file
+    syslog(LOG_DEBUG, "Writing %s to %s.", writestr, writefile);
 
-	if (status != 0)
-	{
-		syslog(LOG_ERR, "Error creating directory %s: %s", dirname(dirc), strerror(errno));
-	}
-
-	fp = fopen (writefile, "a");
-
-	if (fp == NULL)
-	{
-		syslog(LOG_ERR, "Error opening file %s: %s", writefile, strerror(errno));
-		exit(1);
-	}
-
-	fputs (writestr, fp);
-	syslog(LOG_DEBUG, "Writing %s to %s" , writefile, writestr);
-
-	fclose (fp);
-
-	return 0;
+    return 0;
 }
